69f0ec0f623b81f47664ca948f1dbd05dbd3b853
Þórður Pálmi <thordurpalmi2005@gmail.com>
2023-02-04 17:16:56 +0000

    Shooting-v1

--- BullethellGit/Assets/Scripts/PlayerMovement.cs
+++ BullethellGit/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     private Vector2 movement;
 
@@ -15,5 +17,17 @@ public class PlayerMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         transform.position += new Vector3(movement.x, movement.y, 0f) * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * 500f);
     }
 }
