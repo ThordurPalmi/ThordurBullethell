fb0605bcaf0e720399c2912e83ee726e4c620593
Þórður Pálmi <thordurpalmi2005@gmail.com>
2023-02-04 16:48:55 +0000

    characterMovement

---
+++ BullethellGit/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Vector2 movement;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(movement.x, movement.y, 0f) * moveSpeed * Time.deltaTime;
+    }
+}
